99c4dcad5d045693879596aa0f592ca6a9752e5e
´Israel <iaguilaror@gmail.com>
2017-08-21 05:50:22 -0500

    bakup before mk reconvertion

--- DEL/config.mk
+++ DEL/config.mk
@@ -2,14 +2,15 @@
 PATH=/castle/cfresno/.bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/usr/lib/jvm/java-8-oracle/bin:/usr/lib/jvm/java-8-oracle/db/bin:/usr/lib/jvm/java-8-oracle/jre/bin:/usr/lib/plan9/bin:/usr/bin/
 
 #Directorio del genoma de referencia
-REF="/castle/iaguilar/resources/references/human_g1k_v37_decoy.fasta"
+##REF="/castle/iaguilar/resources/references/human_g1k_v37_decoy.fasta"
+REF="/reference/ftp.broadinstitute.org/bundle/hg38/Homo_sapiens_assembly38.fasta"
 
 #Delly2 parametrization
 #Number of threads to run parallel Delly2
-OMP_NUM_THREADS="8"
+OMP_NUM_THREADS="1"
 
 #Number of files per batch for Delly2 analysis
-BATCH_N="2"
+BATCH_N="1"
 
 #Longitud mínima/máxima de eventos registrados
 MIN_LONG="500"
